c975d4848c29bf22ff4e87354a8d8cafa216bf9c
ArtemVikharev <webavikharev@gmail.com>
2025-11-02 18:42:19 +0300

    Реализовал создание объекта из строки итеративным методом

---
+++ second-task.js
@@ -0,0 +1,20 @@
+function buildNestedObjectIterative(str) {
+  const arrItems = str.split(".");
+  let resultObj = {};
+  let currentObj = resultObj;
+
+  for (let index = 0; index < arrItems.length; index++) {
+    currentObj[arrItems[index]] = {};
+    currentObj = currentObj[arrItems[index]];
+  }
+
+  return resultObj;
+}
+
+let resultObjWithIterative = buildNestedObjectIterative(
+  "one.two.three.four.five"
+);
+
+console.log(
+  `Создание объекта через итирации цикла => ${resultObjWithIterative}`
+);

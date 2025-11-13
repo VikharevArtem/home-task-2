60f60a64b0bf02575d1ca8f171f79868fb9af704
ArtemVikharev <webavikharev@gmail.com>
2025-11-13 13:36:40 +0300

    внесены изменения указанные в фидбеке

--- first-task.js
+++ first-task.js
@@ -10,18 +10,23 @@ function sumClosure(initialValue = 0) {
   return innerFn;
 }
 
-let resultSumClosure = Number(sumClosure(2)(3)(5));
+let resultSumClosure = sumClosure(2)(3)(5)(4);
 
 console.log("Сумма через замыкание => " + resultSumClosure);
 
 function sumRecursive(value) {
-  return function (nextValue) {
+  const innerFn = function (nextValue) {
     if (nextValue === undefined) {
       return value;
     }
     return sumRecursive(value + nextValue);
   };
+
+  innerFn.valueOf = () => value;
+
+  return innerFn;
 }
-let resultSumRecursive = sumRecursive(1)(2)(3)(4)(5)();
+
+let resultSumRecursive = sumRecursive(1)(2)(3)(4)(5);
 
 console.log("Сумма через рекурсию  => " + resultSumRecursive);
